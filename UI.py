e744ef5b6c1b0fca68764eb658389f846ae4451e
AlphaIdylSaythTG <123337877+AlphaIdylSaythTG@users.noreply.github.com>
2023-04-26 17:22:09 -0700

    final updates

--- UI.py
+++ UI.py
@@ -20,11 +20,7 @@ if st.button('Sign me up!'):
             st.error('Failed to add user')
     else:
         st.warning('Please enter a name and phone number')
-# if st.button('Get users'):
-#     response = requests.get('http://localhost:5000/get-users')
-#     if response.ok:
-#         users = response.json() #is a list of jsons
-#         st.write(users)
+
 
 
 # i want it to be get-word-users and get-quote-users 
